86a3ce859fbd8093332893db86b521109e9d09d0
admin <admin@PC-2>
2016-06-23 16:50:02 +0530

    Add project files.

---
+++ LazyExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LazyExample
+{
+    class Program
+    {
+        class Test
+        {
+            int[] _array;
+
+            public Test()
+            {
+                Console.WriteLine("Test");
+                _array = new int[5];
+            }
+
+            public int Length
+            {
+                get { return _array.Length; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Lazy<Test> lazy = new Lazy<Test>();
+            Console.WriteLine("IsValueCreated = {0}", lazy.IsValueCreated);
+            Test test = lazy.Value;
+            Console.WriteLine("IsValueCreated = {0}", lazy.IsValueCreated);
+            Console.ReadKey();
+        }
+    }
+}
